01f0bef7927a8e7accbd8a7aa4165b01a0ff6d5b
Mello-Cello <celloelle@gmail.com>
2019-03-29 11:05:36 -0700

    Set up variables for song parts. Wrote lines_for_verse method.

--- 05-alouette/alouette.rb
+++ 05-alouette/alouette.rb
@@ -1,35 +1,21 @@
 class Alouette
-  attr_reader :refrain, :et_array, :allouette, :aaaah
+  refrain = "Alouette, gentille alouette,
+  Alouette, je te plumerai"
 
-  def initialize
-    refrain = "Alouette, gentille alouette,
-Alouette, je te plumerai"
-
-    et_array = ["la tête", "le bec", "les yeux", "le cou", "les ailes", "les pattes", "la queue", "le dos"]
-
-    # et1 = "la tête"
-    # et2 = "le bec"
-    # et3 = "les yeux"
-    # et4 = "le cou"
-    # et5 = "les ailes"
-    # et6 = "les pattes"
-    # et7 = "la queue"
-    # et8 = "le dos"
-
-    allouette = "Alouette!"
-    aaaah = "A-a-a-ah"
-  end
+  ET_ARRAY = ["la tête", "le bec", "les yeux", "le cou", "les ailes", "les pattes", "la queue", "le dos"]
+  
+  allouette = "Alouette!"
+  aaaah = "A-a-a-ah"
 
   def self.lines_for_verse(verse_num)
     # return an array of strings, without repeats
     # example, lines_for_verse(2):
     # ["Et les yeux!", "Et le bec!", "Et la tête!"]
 
-    # Alouette.new # <-- do I need to create an instance of this class?
     i = 0
     lines_for_verse = []
     (verse_num + 1).times do
-      lines_for_verse.insert(0, "Et " + @et_array[i] + " !")
+      lines_for_verse.insert(0, "Et #{ET_ARRAY[i]}!")
       i += 1
     end
     return lines_for_verse
@@ -46,3 +32,26 @@ Alouette, je te plumerai"
     # returns a string
   end
 end
+
+# -------------------
+
+# et1 = "la tête"
+# et2 = "le bec"
+# et3 = "les yeux"
+# et4 = "le cou"
+# et5 = "les ailes"
+# et6 = "les pattes"
+# et7 = "la queue"
+# et8 = "le dos"
+
+#     attr_reader :refrain, :et_array, :allouette, :aaaah
+
+#   def initialize
+#     refrain = "Alouette, gentille alouette,
+# Alouette, je te plumerai"
+
+#     et_array = ["la tête", "le bec", "les yeux", "le cou", "les ailes", "les pattes", "la queue", "le dos"]
+
+#     allouette = "Alouette!"
+#     aaaah = "A-a-a-ah"
+#   end
